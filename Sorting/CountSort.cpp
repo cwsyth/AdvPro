ab12b0fd722b2fb4947bc365cbf19162524686f6
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2025-01-08 01:12:14 +0100

    update countsort

--- Sorting/CountSort.cpp
+++ Sorting/CountSort.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <string>
-
-const int domainSize = 10;
 
 struct Person {
     int key;
     std::string name;
 };
+const int domain_size= 10;
 
-std::vector<Person> countSort(std::vector<Person> v1) {
-    std::vector<int> c(domainSize);
+std::vector<Person> countSortStable(std::vector<Person> v1) {
+    std::vector<int> c(domain_size);
     std::vector<Person> v2(v1.size());
 
     // count keys
-    for(Person person : v1) {
-        c[person.key - 1] = c[person.key - 1] + 1;
+    for (Person p : v1) {
+        c[p.key - 1]++;
     }
 
     // comulative sum
-    for(int i = 1; i < domainSize; i++) {
+    for (int i = 1; i < domain_size; i++) {
         c[i] = c[i] + c[i - 1];
     }
 
     // place in correct order
-    for(int i = v1.size() - 1; i >= 0; i--) {
+    for (int i = v1.size() - 1; i >= 0; i--) {
         int cIndex = v1[i].key - 1;
         v2[c[cIndex] - 1] = v1[i];
-        c[cIndex] = c[cIndex] - 1;
+        c[cIndex]--;
     }
 
     return v2;
@@ -44,11 +42,11 @@ int main() {
         {10, "Edward"}
     };
 
-    std::vector<Person> sortedPersons = countSort(persons);
+    std::vector<Person> sortedPersons = countSortStable(persons);
 
     std::cout << "Sorted Persons by Key:\n";
-    for(Person person : sortedPersons) {
-        std::cout << "Key: " << person.key << ", Name: " << person.name << std::endl;
+    for (Person person : sortedPersons) {
+        std::cout << "Key: " << person.key << ", Name: " << person.name << '\n';
     }
 
     return 0;
