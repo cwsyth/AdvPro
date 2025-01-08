775b6f12bb1b8a7799d398eccc56eef4b0f6d325
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2025-01-08 03:23:17 +0100

    refactor bucketSort to modify input vector in place

--- Sorting/BucketSort.cpp
+++ Sorting/BucketSort.cpp
@@ -2,30 +2,29 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<float> bucketSort(std::vector<float> v1) {
-    std::vector<std::vector<float>> bucket(v1.size());
-    std::vector<float> v2;
+void bucketSort(std::vector<float> &v) {
+    std::vector<std::vector<float>> bucket(v.size());
 
-    for(int i = 0; i < v1.size(); i++) {
-        int bucketIndex = static_cast<int> (v1.size() * (v1[i] + 1000) / 2000); // shift + divide + convert;
-        bucket[bucketIndex].push_back(v1[i]);
+    for(int i = 0; i < v.size(); i++) {
+        int bucketIndex = static_cast<int> (v.size() * (v[i] + 1000) / 2000); // shift + divide + convert;
+        bucket[bucketIndex].push_back(v[i]);
     }
 
-    // concat
-    for(int i = 0; i < v1.size(); ++i) {
-        sort(bucket[i].begin(), bucket[i].end());
-        v2.insert(v2.end(), bucket[i].begin(), bucket[i].end());
+    v.clear();
+    for(int i = 0; i < bucket.size(); ++i) {
+       std::sort(bucket[i].begin(), bucket[i].end());
+        
+        v.insert(v.end(), bucket[i].begin(), bucket[i].end());
     }
-
-    return v2;
 }
+
 
 int main() {
     std::vector<float> vec = { 900.5, -200.3, 0.0, 500.4, -999.9, 250.1, -100.2 };
     
-    std::vector<float> sortedVec = bucketSort(vec);
+    bucketSort(vec);
     
-    for(float f : sortedVec) {
+    for (float f : vec) {
         std::cout << f << ", ";
     }
 
