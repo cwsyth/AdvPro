92ff6d7b281605bf3e8e1a4e2fafa14866ac9f91
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2025-01-09 19:28:16 +0100

    correct comment

--- Efficiency/integer_multiplication.cpp
+++ Efficiency/integer_multiplication.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-// x*5  = x * x * x * x * x = x*2 + x*2 + x = x << 2 + x
+// *+5 = x + x + x + x + x = x*2 + x*2 + x = x << 2 + x
 int multiply_by_five(unsigned int x) {
     return (x << 2) + x;
 }
